1ec187ca296504d2816f903777a98ec86f8d5a39
sweak <sweakpolska@gmail.com>
2021-06-21 18:49:40 +0200

    Added 3 new alarm tones (annoying ones)

--- README.md
+++ README.md
@@ -24,6 +24,9 @@ No more oversleeping with *SmartAlarm*! Download the default QR code and print i
 
 #### Sounds attribution
 * "Alarm Clock" by Mike Koenig http://soundbible.com/535-Alarm-Clock.html License: Attribution 3.0
+* "Air Horn" by Mike Koenig https://soundbible.com/1542-Air-Horn.html License: Attribution 3.0
 * "Analog Watch Alarm" by Daniel Simion http://soundbible.com/2197-Analog-Watch-Alarm.html License: Attribution 3.0
+* "Airhorn" by guitarguy1985 https://soundbible.com/1080-Airhorn.html License: Sampling Plus 1.0
+* "Siren Noise" by KevanGC https://soundbible.com/1577-Siren-Noise.html License: Public Domain
 * "Sunny Day" by stephan http://soundbible.com/1661-Sunny-Day.html License: Public Domain
 * "Gentle Guitar" by Rasputin https://www.looperman.com/loops/detail/230868/gentle-guitar-77bpm-folk-acoustic-guitar-loop
--- app/src/main/java/com/sweak/smartalarm/util/AlarmToneManager.java
+++ app/src/main/java/com/sweak/smartalarm/util/AlarmToneManager.java
@@ -9,6 +9,9 @@ public class AlarmToneManager {
     public static final int SUNNY_DAY = 2;
     public static final int ALARM_CLOCK = 3;
     public static final int ANALOG_WATCH = 4;
+    public static final int AIR_HORN_SHORT = 5;
+    public static final int AIR_HORN_LONG = 6;
+    public static final int SIREN_NOISE = 7;
 
     public static Uri getAlarmToneUri(int alarmType) {
         switch (alarmType) {
@@ -20,6 +23,12 @@ public class AlarmToneManager {
                 return Uri.parse("android.resource://com.sweak.smartalarm/raw/alarm_clock");
             case ANALOG_WATCH:
                 return Uri.parse("android.resource://com.sweak.smartalarm/raw/analog_watch");
+            case AIR_HORN_SHORT:
+                return Uri.parse("android.resource://com.sweak.smartalarm/raw/air_horn_short");
+            case AIR_HORN_LONG:
+                return Uri.parse("android.resource://com.sweak.smartalarm/raw/air_horn_long");
+            case SIREN_NOISE:
+                return Uri.parse("android.resource://com.sweak.smartalarm/raw/siren_noise");
             case DEFAULT_SYSTEM:
             default:
                 return RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
